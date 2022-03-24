71e837d31c1229e55d2c6f67fa0b1949946a6c86
Luiz Felipe Warmling Amadeu <85834483+Amad3eu@users.noreply.github.com>
2022-03-24 18:44:49 -0300

    Add files via upload

---
+++ App/src/script/style.js
@@ -0,0 +1,50 @@
+// Function for scrolling header
+
+function scrollHeader() {
+    var header = document.getElementById('header'),
+        headerHeight = header.offsetHeight, // Header height
+        content = document.getElementById('content'),
+        previousScroll = content.scrollTop; // Get onload Scrolling
+
+    content.onscroll = function() {
+        currentScroll = content.scrollTop; // Get the current scrolling position
+
+        // Hide the header only if the header height is scroll
+        if (currentScroll > headerHeight) {
+            if (currentScroll > previousScroll) {
+                header.style.transform = 'translateY(-' + headerHeight + 'px)'; // Hide header
+            } else {
+                header.style.transform = 'translateY(0)'; // Show header
+            }
+        }
+        previousScroll = currentScroll;
+    };
+}
+scrollHeader(); //init function
+
+
+// Function to toggle the menu 
+
+function toggleMenu() {
+    var navToggle = document.getElementById('nav-toggle');
+
+    navToggle.onclick = function() {
+        document.body.classList.toggle("open");
+    };
+}
+toggleMenu(); // init function
+
+// Function for real clock
+
+function updateClock() {
+    var now = new Date(), // current date
+        time = now.getHours() + ':' + now.getMinutes(),
+        target = document.getElementById('time');
+
+    // set the content of the element with the ID time to the formatted string
+    target.innerHTML = [time];
+
+    // call this function again in 1000ms
+    setTimeout(updateClock, 1000);
+}
+updateClock(); // init function
