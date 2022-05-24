76507f86df00c8b86296426b39c284540c2bdf44
Luiz Felipe Warmling Amadeu <85834483+Amad3eu@users.noreply.github.com>
2022-05-24 19:46:27 -0300

    add files principals

--- App/src/script/index.js
+++ App/src/script/index.js
@@ -90,3 +90,85 @@ $(".previous").click(function () {
 $(".submit").click(function () {
   return false;
 });
+
+const fields = document.querySelectorAll("[required]");
+
+function ValidateField(field) {
+  // logica para verificar se existem erros
+  function verifyErrors() {
+    let foundError = false;
+
+    for (let error in field.validity) {
+      // se não for customError
+      // então verifica se tem erro
+      if (field.validity[error] && !field.validity.valid) {
+        foundError = error;
+      }
+    }
+    return foundError;
+  }
+
+  function customMessage(typeError) {
+    const messages = {
+      text: {
+        valueMissing: "Por favor, preencha este campo",
+      },
+      email: {
+        valueMissing: "Email é obrigatório",
+        typeMismatch: "Por favor, preencha um email válido",
+      },
+    };
+
+    return messages[field.type][typeError];
+  }
+
+  function setCustomMessage(message) {
+    const spanError = field.parentNode.querySelector("span.error");
+
+    if (message) {
+      spanError.classList.add("active");
+      spanError.innerHTML = message;
+    } else {
+      spanError.classList.remove("active");
+      spanError.innerHTML = "";
+    }
+  }
+
+  return function () {
+    const error = verifyErrors();
+
+    if (error) {
+      const message = customMessage(error);
+
+      field.style.borderColor = "red";
+      setCustomMessage(message);
+    } else {
+      field.style.borderColor = "green";
+      setCustomMessage();
+    }
+  };
+}
+
+function customValidation(event) {
+  const field = event.target;
+  const validation = ValidateField(field);
+
+  validation();
+}
+
+for (field of fields) {
+  field.addEventListener("invalid", (event) => {
+    // eliminar o bubble
+    event.preventDefault();
+
+    customValidation(event);
+  });
+  field.addEventListener("blur", customValidation);
+}
+
+document.querySelector("form").addEventListener("submit", (event) => {
+  console.log("enviar o formulário");
+
+  // não vai enviar o formulário
+  event.preventDefault();
+});
