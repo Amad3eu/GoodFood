899a8a5a78cb0bd644300386d56be93a38e0c19d
Amad3eu <luizfelipewarmling@gmail.com>
2022-06-30 14:47:50 -0300

    requests part finalized

---
+++ .request/js/models.js
@@ -0,0 +1,7 @@
+let modelsJson = [
+    {id:1, name:'Vanilla Latte', img:'/.request/img/quente-vanilla-latte.jpg', price:[10.7, 11.7, 13.7], sizes:['1/72', '1/48', '1/32'], description:'Latte and Vanilla. Addiction with capital A (with coffee and milk) (230ml)'},
+    {id:2, name:'Urban Chocolat', img:'/.request/img/quente-urban-chocolat.jpg', price:[8.7, 9.7, 11.7], sizes:['1/72', '1/48', '1/32'], description:'Is this the best hot chocolate in the world? (without coffee, with milk) (230ml)'},
+    {id:3, name:'Mad Mocha', img:'/.request/img/quente-madmocha.jpg', price:[10.1, 11.1, 13.1], sizes:['1/72', '1/48', '1/32'], description:'Coffee and chocolate. A piece of heaven on earth. (with coffee and milk) (230ml)'},
+    {id:4, name:'Flat Caramel', img:'/.request/img/quente-flat-caramel.jpg', price:[10.8, 11.8, 13.8], sizes:['1/72', '1/48', '1/32'], description:'Latte with caramel. The best thing you will taste today. (With coffee and milk) (230ml)'},
+    {id:5, name:'Matcha Latte', img:'/.request/img/quente-matcha-latte.jpg', price:[8.9, 9.9, 11.9], sizes:['1/72', '1/48', '1/32'], description:'Matcha and steamed milk to perfection. An age-old drink. (without coffee, with milk) (230ml)'},
+];
