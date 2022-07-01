650febe067b3f224886a73c71c453914c69249ac
Amad3eu <luizfelipewarmling@gmail.com>
2022-07-01 18:04:31 -0300

    alter bag-coffe page

--- .request/areacd/bag-coffee/js/models.js
+++ .request/areacd/bag-coffee/js/models.js
@@ -1,5 +1,5 @@
 let modelsJson = [
     {id:1, name:'White Bag', img:'/.request/img/bag-coffee/cafe_torrado_moido_250g.jpeg', price:[32, 64, 95], sizes:['250g', '500g', '1kg'], description:'Special coffee, 80 - 85 points'},
-    {id:2, name:'Kraft Edition', img:'/.request/img/bag-coffee/coffeebag(black)-250g.jpg', price:[42, 85, 120], sizes:['250g', '500g', '1kg'], description:'Specialty Coffee - Single Origin - 85 to 87'},
-    {id:3, name:'Black Edition', img:'/.request/img/bag-coffee/coffeebag(kraft)-250g.jpg', price:[54, 110, 140], sizes:['250g', '500g', '1kg'], description:'Specialty Coffee - Single Origin - 88 points or more'},
+    {id:2, name:'Kraft Edition', img:'/.request/img/bag-coffee/coffeebag(kraft)-250g.jpg', price:[42, 85, 120], sizes:['250g', '500g', '1kg'], description:'Specialty Coffee - Single Origin - 85 to 87'},
+    {id:3, name:'Black Edition', img:'/.request/img/bag-coffee/coffeebag(black)-250g.jpg', price:[54, 110, 140], sizes:['250g', '500g', '1kg'], description:'Specialty Coffee - Single Origin - 88 points or more'},
 ];
