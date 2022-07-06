ab316c03411dd82f2db0d45a26b08d66371cb159
Amad3eu <luizfelipewarmling@gmail.com>
2022-07-06 12:10:14 -0300

    add page for burguers and alteration sizes in areacd

--- .request/areacd/cold/js/models.js
+++ .request/areacd/cold/js/models.js
@@ -1,7 +1,7 @@
 let modelsJson = [
-    {id:1, name:'Iced Vanilla', img:'/.request/img/cold/gelado-iced-vanilla.jpg', price:[12.4, 13.4, 15.4], sizes:['1/72', '1/48', '1/32'], description:'The perfect combination of coffee, milk and vanilla. Colder than your heart. (With coffee and milk) (330ml)'},
-    {id:2, name:'Salted Caramel', img:'/.request/img/cold/gelado-salted-caramel.jpg', price:[14.1, 15.1, 17.1], sizes:['1/72', '1/48', '1/32'], description:'Cold latte with salted caramel. (With coffee and milk) (330ml)'},
-    {id:3, name:'Black Ginger', img:'/.request/img/cold/gelado-black-ginger.jpg', price:[13.4, 14.4, 16.4], sizes:['1/72', '1/48', '1/32'], description:'Espresso with ice, tonic and ginger magic. Surreal good, Ooh yeah. (With coffee, without milk) (330ml)'},
-    {id:4, name:'Matcha Iced Latte', img:'/.request/img/cold/gelado-matcha-iced-latte.jpg', price:[12.3, 13.3, 15.3], sizes:['1/72', '1/48', '1/32'], description:'Matcha, milk and a touch of mint. Cold and mesmerizing. (Without coffee, with milk) (330ml)'},
-    {id:5, name:'Chocold Mint', img:'/.request/img/cold/gelado-chocold-mint.jpg', price:[10.8, 11.8, 13.8], sizes:['1/72', '1/48', '1/32'], description:'Iced chocolate and mint. No, you are not dreaming. (Without coffee, with milk) (330ml)'},
+    {id:1, name:'Iced Vanilla', img:'/.request/img/cold/gelado-iced-vanilla.jpg', price:[12.4, 13.4, 15.4], sizes:['P','M', 'G'], description:'The perfect combination of coffee, milk and vanilla. Colder than your heart. (With coffee and milk) (330ml)'},
+    {id:2, name:'Salted Caramel', img:'/.request/img/cold/gelado-salted-caramel.jpg', price:[14.1, 15.1, 17.1], sizes:['P','M', 'G'], description:'Cold latte with salted caramel. (With coffee and milk) (330ml)'},
+    {id:3, name:'Black Ginger', img:'/.request/img/cold/gelado-black-ginger.jpg', price:[13.4, 14.4, 16.4], sizes:['P','M', 'G'], description:'Espresso with ice, tonic and ginger magic. Surreal good, Ooh yeah. (With coffee, without milk) (330ml)'},
+    {id:4, name:'Matcha Iced Latte', img:'/.request/img/cold/gelado-matcha-iced-latte.jpg', price:[12.3, 13.3, 15.3], sizes:['P','M', 'G'], description:'Matcha, milk and a touch of mint. Cold and mesmerizing. (Without coffee, with milk) (330ml)'},
+    {id:5, name:'Chocold Mint', img:'/.request/img/cold/gelado-chocold-mint.jpg', price:[10.8, 11.8, 13.8], sizes:['P','M', 'G'], description:'Iced chocolate and mint. No, you are not dreaming. (Without coffee, with milk) (330ml)'},
 ];
